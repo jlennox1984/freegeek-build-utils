e70bf57d76f02fd53e53e12f099d07838f939566
Ryan Niebur <ryanryan52@gmail.com>
2011-02-24 13:16:08 -0800

    fixup some translated text, and add some things. remove the .pot file from git.

--- po/generate.sh
+++ po/generate.sh
@@ -3,7 +3,7 @@
 set -e
 
 rm -f po/basiccheck.pot
-ruby -r yaml -e "YAML::load(File.read('basicchecks.yml'))['tests'].values.map{|x| [:title, :success, :failure, :pretest, :explanation].map{|y| x[y.to_s]}}.flatten.select{|x| !x.nil?}.uniq.each{|x| puts \"a = _(#{x.inspect})\"}" > po/tmpfile.rb
+ruby -r yaml -e "YAML::load(File.read('basicchecks.yml'))['tests'].values.map{|x| [:title, :success, :failure, :pretest, :explanation, :question].map{|y| x[y.to_s]}}.flatten.select{|x| !x.nil?}.uniq.each{|x| puts \"a = _(#{x.inspect})\"}" > po/tmpfile.rb
 rgettext basiccheck po/tmpfile.rb -o po/basiccheck.pot
 rm -f po/tmpfile.rb
 for FILE in po/*.po; do
