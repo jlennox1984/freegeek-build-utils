ba4102ffa13beac374082f751c92dd8ef79442e1
Ryan Niebur <ryanryan52@gmail.com>
2011-03-13 22:49:06 -0700

    publish.sh: in the future, make a backup

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+ssh ryan52@llama.freegeek.org mv public_html/mac/printme public_html/mac/printme.old
 { head -1 ../freegeek-extras/scripts/printme; cat ../rubytui/lib/rubytui.rb ../freegeek-extras/lib/printme.rb ../freegeek-extras/scripts/printme; } | sed -e '/require.*printme/ d' -e '/require.*rubytui/ d' | ssh ryan52@llama.freegeek.org sudo sponge public_html/mac/printme
