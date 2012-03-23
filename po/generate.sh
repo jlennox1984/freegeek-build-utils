f79ca20effc064e2a5c3091978560726f99592c8
Ryan Niebur <ryanryan52@gmail.com>
2012-03-23 12:56:39 -0700

    the generate.sh to create po files with the new basicchecks.yml format

--- po/generate.sh
+++ po/generate.sh
@@ -3,7 +3,8 @@
 set -e
 
 rm -f po/basiccheck.pot
-ruby -r yaml -e "YAML::load(File.read('basicchecks.yml'))['tests'].values.map{|x| [:title, :success, :failure, :pretest, :explanation, :question].map{|y| x[y.to_s]}}.flatten.select{|x| !x.nil?}.uniq.each{|x| puts \"a = _(#{x.inspect})\"}" > po/tmpfile.rb
+                                                                 # .values.map, previously
+ruby -r yaml -e "YAML::load(File.read('basicchecks.yml'))['tests'].map{|x| [:title, :success, :failure, :pretest, :explanation, :question].map{|y| x[y.to_s]}}.flatten.select{|x| !x.nil?}.uniq.each{|x| puts \"a = _(#{x.inspect})\"}" > po/tmpfile.rb
 ruby -r gettext/tools/rgettext -e GetText.rgettext -- basiccheck po/tmpfile.rb -o po/basiccheck.pot
 rm -f po/tmpfile.rb
 for FILE in po/*.po; do
